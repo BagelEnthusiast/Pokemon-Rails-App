1be62c491d0015c35756c07dfa89c1536e91671a
BagelEnthusiast <nathan.richardson94@gmail.com>
2019-05-07 14:18:37 -0500

    Seed for Pokemon and Moves

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'mini_racer', platforms: :ruby
+gem 'rest-client'
 
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.2'
@@ -40,6 +41,8 @@ group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
 end
+
+
 
 group :development do
   # Access an interactive console on exception pages or by calling 'console' anywhere in the code.
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,47 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "rest-client"
+require "json"
+
+Pokemon.destroy_all
+Move.destroy_all
+
+response_string = RestClient.get("https://pokeapi.co/api/v2/")
+response_hash = JSON.parse(response_string)
+
+#Pokemon Seed Data
+pokemon_response_string = RestClient.get("https://pokeapi.co/api/v2/pokemon/?limit=10")
+pokemon_response_hash = JSON.parse(pokemon_response_string)
+pokemon_arr = pokemon_response_hash["results"]
+
+pokemon_arr.each do |pokemon|
+    pokemon_info = RestClient.get(pokemon["url"])
+    pokemon_info_hash = JSON.parse(pokemon_info)
+
+  Pokemon.create(
+    name: pokemon["name"],
+    base_experience: pokemon_info_hash["base_experience"],
+    height: pokemon_info_hash["height"],
+    order: pokemon_info_hash["order"],
+    weight: pokemon_info_hash["weight"]
+  )
+end
+
+#Move Seed Data
+move_response_string = RestClient.get("https://pokeapi.co/api/v2/move/?limit=10")
+move_response_hash = JSON.parse(move_response_string)
+move_arr = move_response_hash["results"]
+
+move_arr.each do |move|
+    move_info = RestClient.get(move["url"])
+    move_info_hash = JSON.parse(move_info)
+
+  Move.create(
+    name: move["name"],
+    accuracy: move_info_hash["accuracy"],
+    effect_chance: move_info_hash["effect_chance"],
+    pp: move_info_hash["pp"],
+    priority: move_info_hash["priority"],
+    power: move_info_hash["power"],
+    effect: move_info_hash["effect_entries"][0]["effect"]
+  )
+end
+
